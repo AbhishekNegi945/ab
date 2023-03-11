3842ca4e3f3baf3270a617a6bb77bf4b171e19ec
AbhishekNegi945 <124780669+AbhishekNegi945@users.noreply.github.com>
2023-03-11 15:39:33 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+
+
+{
+    char s1[]="ABHISHEK ";
+    char s2[]="negi";
+
+    strcat(s1,s2);
+    printf("%s",s1);
+
+
+    
+
+
+    return 0;
+}
